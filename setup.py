7bef106361fff0e2858fd40ea159379e5c1e6710
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-07-06 13:08:27 -0500

    ADD Actividad 26

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="repositorio_actividad_26",
+    name="junal-actividad-26-2025",
     version="0.1.0",
-    packages=find_packages(),
+    packages=["junal_actividad_26"],
     install_requires=[],
-    author="Junal Chowdhury",
-    description="Repo Actividad 26",
+    author="JunalChowdhuryG",
+    author_email="chowdhurygomezjunaljohir@gmail.com",  # Añade tu correo
+    description="prueba para Actividad 26",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/JunalChowdhuryG/Actividad-26",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
